a32331495296fc8a491af463c3f832602f96a958
Arpit Thakur <arpitthakut722@gmail.com>
2020-05-19 14:38:28 +0530

    recepie card

---
+++ recepie card/index.js
@@ -0,0 +1,11 @@
+var recepie = {
+    'title':'Omelette',
+    'servings':2,
+    'ingredients':['4eggs','2strips of bacon','1 tsp salt/pepper']
+};
+console.log(recepie.title);
+console.log('servings:'+ recepie.servings);
+console.log('ingredients:');
+for (var i = 0;i<ingredients.length;i++){
+    console.log(recepie.ingredients);
+}
