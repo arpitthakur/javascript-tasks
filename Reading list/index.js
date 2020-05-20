8a8a6d1da7dba59db38a0e6efaf301348fe01b28
Arpit Thakur <arpitthakut722@gmail.com>
2020-05-20 12:13:12 +0530

    Who wants a drink

--- Reading list/index.js
+++ Reading list/index.js
@@ -9,15 +9,15 @@ var books = [
     alreadyRead: true
 }];
 for (var i = 0; i < books.length; i++) {
-    var book = books[i];
-    var bookInfo = book.title + '" by ' + book.author;
-    if (book.alreadyRead) {
-      console.log('You already read "' + bookInfo);
-    } else {
-      console.log('You still need to read "' + bookInfo);
-    }
+  var book = books[i];
+  var bookInfo = book.title + '" by ' + book.author;
+  if (book.alreadyRead) {
+    console.log('You already read "' + bookInfo);
+  } else {
+    console.log('You still need to read "' + bookInfo);
   }
-  
+}
+
 
 
   
