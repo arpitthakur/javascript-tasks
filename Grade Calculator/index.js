6a667c4f2acc3cf5cd04e1474370499777aa256e
Arpit Thakur <arpitthakut722@gmail.com>
2020-05-26 16:42:11 +0530

    javascript file of You are amazing noer

--- Grade Calculator/index.js
+++ Grade Calculator/index.js
@@ -1,6 +1,17 @@
-function setgrades(){
-    var obtained = 60;
-    var total = 100;
-    var percent = obtained*100/total;
-console.log(percent);
-}
+let grade = 67
+if(grade <= 100 && grade >= 90){
+    console.log('You got a ' + 'A' + '(' + grade + '%' + ')!')
+}else if(grade >=80){
+    console.log('You got a ' + 'B' + '(' + grade + '%' + ')!')
+}else if(grade >=70){
+    console.log('You got a ' + 'C' + '(' + grade + '%' + ')!')
+}else if(grade >=60){
+    console.log('You got a ' + 'D' + '(' + grade + '%' + ')!')
+}else if(grade >=50){
+    console.log('You got a ' + 'E' + '(' + grade + '%' + ')!')
+}else if (grade >=0){
+    console.log('You got a' + 'F' + '(' + grade + '%' +')!')
+}else {
+    console.log('You have entered the wrong value')
+
+};
