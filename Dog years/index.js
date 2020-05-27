8ad8633469348ad1963942d79dd91c7465b4aab8
Arpit Thakur <arpitthakut722@gmail.com>
2020-05-27 11:19:16 +0530

    javascript file of Dog years

---
+++ Dog years/index.js
@@ -0,0 +1,7 @@
+function calculateDogAge (age) {
+    var dogYears = 7* age
+    console.log("Your doggie is" + dogYears + "year old in dog years!");
+}
+calculateDogAge(1);
+calculateDogAge(2);
+calculateDogAge(3);
