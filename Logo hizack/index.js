745e528091f053325aabb530e74d5641d054bfa7
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-03 10:54:19 +0530

    initial commit

--- Logo hizack/index.js
+++ Logo hizack/index.js
@@ -1,3 +1,2 @@
-var logo = document.getElementById('logo-default')
+var logo = document.getElementById("logo-default")
 logo.src = "https://www.hackyourfuture.dk/static/logo-dark.svg"
-logo.srcset = "https://www.hackyourfuture.dk/static/logo-dark.svg"
