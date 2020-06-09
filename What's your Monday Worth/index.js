b165ed165ff955978c7a2ee1d58ec8773e187d91
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-09 12:36:14 +0530

    half file of what's your Monday

---
+++ What's your Monday Worth/index.js
@@ -0,0 +1,18 @@
+const mondayTasks = [
+  {
+    name: "Daily standup",
+    duration: 30, // specified in minutes
+  },
+  {
+    name: "Feature discussion",
+    duration: 120,
+  },
+  {
+    name: "Development time",
+    duration: 240,
+  },
+  {
+    name: "Talk to different members from the product team",
+    duration: 60,
+  },
+];
