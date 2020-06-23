06c2d2f05502d062048640eac187f86a57a3d4b9
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-23 11:35:01 +0530

    full file of index.js

--- Add Six/index.js
+++ Add Six/index.js
@@ -1,7 +1,5 @@
 function createBase(baseNumber) {
     return function(N) {
-      // we are referencing baseNumber here even though it was declared
-      // outside of this function. Closures allow us to do this in JavaScript
       return baseNumber + N;
     }
   }
