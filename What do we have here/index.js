f94fa4fc9a9823f0a6c92215ce068a951589ef98
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-26 15:36:02 +0530

    added full file of index.js

---
+++ What do we have here/index.js
@@ -0,0 +1,26 @@
+function reqListener(){
+    const facts = JSON.parse(this.responseText);
+    console.log(facts);
+}
+var xhr = new XMLHttpRequest();
+console.log("initializing XMLHttpRequest",xhr);
+xhr.addEventListener("load",reqListener);
+xhr.open('GET','https://www.randomuser.me/api');
+console.log("making a request...")
+
+xhr.send();
+console.log("sent the request,let's see... ");
+console.log(axious);
+axious.get('https://www.randomuser.me/api')
+
+.then(function (response) {
+    console.log('GetV', response);
+})
+.catch(function (error) {
+    console.log('an error???????????', error);
+})
+.finally(function () {
+    
+    console.log('Hey!'
+    )
+});  
